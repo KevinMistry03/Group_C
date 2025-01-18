3709ae16f9eb446e390bc93c9a938e1f2af67912
murtazarangwala99 <murtazarangwala95@gmail.com>
2025-01-18 21:34:24 +0530

    get user name added

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 console.log("hello");
-console.log("Hi")
+console.log("Hi");
+
+let firstName;
+let lastName;
+
+function getUserFullName(firstName = "Murtuza", lastName = "Rangwala") {
+  console.log(`${firstName} ${lastName}`);
+}
+
+getUserFullName();
+getUserFullName("Murtuza", "Master");
